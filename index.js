a0f692ad22b74db49923316e5b2cad1707c89347
Collins Ngetich <collinsngetichk@gmail.com>
2024-03-23 21:04:18 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4 === 4 ? num3 % num4 : (num3 % num4) + num4;
+
+console.log(mod);
+
+let numbers = [10, 5, 19, 14, 20, 15];
+let max = Math.max(...numbers);
+
+console.log(max);
